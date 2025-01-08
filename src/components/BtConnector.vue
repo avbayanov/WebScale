dc7f00770144c340d9e4120bd33bc8166f64222e
Alexander Bayanov <bin@bayanov.me>
2025-01-09 00:39:48 +0700

    Missed file

---
+++ src/components/BtConnector.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+  
+
+  function init () {
+    /* navigator.permissions.query({ name: PermissonName.Bluetooth })
+      .then(status => {
+        console.log(status)
+
+        if (status.state === 'denied') {
+          // ask perm
+          // return
+        }
+      }) */
+
+    let options = {
+
+    }
+
+    navigator.bluetooth
+      .requestDevice(options)
+      .then(device => {
+        console.log(`Name: ${device.name}`)
+        // Do something with the device.
+      })
+      .catch(error => console.error(`Something went wrong. ${error}`))
+  }
+
+  init()
+
+  interface BluetoothDevice {
+    name: string
+  }
+</script>
